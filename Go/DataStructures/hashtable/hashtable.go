912f65c5b3568bc10cc8a9074281ec67a9458722
Vladyslav <lefttwixwand@gmail.com>
2021-02-15 23:28:07 +0200

    Added tabulation for printing.

--- Go/DataStructures/hashtable/hashtable.go
+++ Go/DataStructures/hashtable/hashtable.go
@@ -2,22 +2,26 @@ package hashtable
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 )
 
 // HashTable is a data structure
 type HashTable struct {
-	data []*KeyValuePair
+	data     []*KeyValuePair
+	Capacity int
 }
 
 // New is a constructor for the HashTable structure
-func New(size int) *HashTable {
+func (table *HashTable) New(size int) *HashTable {
 	return &HashTable{
-		data: make([]*KeyValuePair, size),
+		data:     make([]*KeyValuePair, size),
+		Capacity: size,
 	}
 }
 
 // Get returns a value of a specific key
-func (table *HashTable) Get(key interface{}) interface{} {
+func (table *HashTable) Get(key int) interface{} {
 	item := table.getNodeByHash(table.hash(key))
 
 	if item != nil {
@@ -27,19 +31,27 @@ func (table *HashTable) Get(key interface{}) interface{} {
 	return nil
 }
 
-func (table *HashTable) getNodeByHash(hash uint64) *KeyValuePair {
-	return table.data[int(hash)]
+func (table *HashTable) getNodeByHash(hash int) *KeyValuePair {
+	return table.data[hash]
 }
 
 // hash is a method, which returns a hash for the specific key
-func (table *HashTable) hash(key interface{}) uint64 {
-	return 1
+func (table *HashTable) hash(key int) int {
+	return (key % table.Capacity) / 100
 }
 
 // Print is a test method for printion all the table
 func (table *HashTable) Print() {
 
-	for _, item := range table.data {
-		fmt.Println("Key = ", item.Key, "Value = ", item.Value)
-	}
+	// for _, item := range table.data {
+	// 	fmt.Println("Key = ", item.Key, "Value = ", item.Value)
+	// }
+
+	const padding = 3
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, '-', tabwriter.AlignRight|tabwriter.Debug)
+	fmt.Fprintln(w, "a\tb\taligned\t")
+	fmt.Fprintln(w, "aa\tbb\taligned\t")
+	fmt.Fprintln(w, "aaa\tbbb\tunaligned") // no trailing tab
+	fmt.Fprintln(w, "aaaa\tbbbb\taligned\t")
+	w.Flush()
 }
--- Go/DataStructures/hashtable/kayvaluepair.go
+++ Go/DataStructures/hashtable/kayvaluepair.go
@@ -2,12 +2,12 @@ package hashtable
 
 // KeyValuePair is a data structude for a Chaining Hash Table
 type KeyValuePair struct {
-	Key   string
+	Key   int
 	Value interface{}
 	Next  *KeyValuePair
 }
 
 // NewKeyValuePair is a constructor for the KeyValuePair structure
-func NewKeyValuePair(key string, value interface{}) *KeyValuePair {
+func NewKeyValuePair(key int, value interface{}) *KeyValuePair {
 	return &KeyValuePair{Key: key, Value: value}
 }
