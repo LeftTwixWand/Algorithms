f6d59e90e47cc0c89fe84e777f9c095401e0fe6d
Vladyslav <lefttwixwand@gmail.com>
2021-02-21 23:57:49 +0200

    Add Insert method to the golang binary tree

--- Go/DataStructures/binarytree/binarytree.go
+++ Go/DataStructures/binarytree/binarytree.go
@@ -11,3 +11,14 @@ func New(root *Node) *BinaryTree {
 		Root: root,
 	}
 }
+
+// Insert allows to add new item to the tree
+func (tree *BinaryTree) Insert(item int) (bool, error) {
+
+	if tree.Root == nil {
+		tree.Root = NewNode(item)
+		return true, nil
+	}
+
+	return tree.Root.Insert(item)
+}
--- Go/DataStructures/binarytree/node.go
+++ Go/DataStructures/binarytree/node.go
@@ -1,5 +1,9 @@
 package binarytree
 
+import (
+	"errors"
+)
+
 // Node is a data structure for the binatytree
 type Node struct {
 	Value int
@@ -12,4 +16,34 @@ func NewNode(data int) *Node {
 	return &Node{
 		Value: data,
 	}
+}
+
+// Insert is a mathod, which allows to insert a new value to the tree
+func (node *Node) Insert(item int) (bool, error) {
+
+	if node.Value == item {
+		return false, errors.New("This value is already exist in a tree")
+	}
+
+	if node.Value > item {
+
+		if node.Left == nil {
+			node.Left = NewNode(item)
+			return true, nil
+		}
+
+		return node.Left.Insert(item)
+	}
+
+	if node.Value < item {
+
+		if node.Right == nil {
+			node.Right = NewNode(item)
+			return true, nil
+		}
+
+		return node.Right.Insert(item)
+	}
+
+	return false, errors.New("Error in processing")
 }
